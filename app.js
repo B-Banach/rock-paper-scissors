4414319ac2d44a1d25efe4759fe2cc2922926e96
Bartek-B <bartlomiej.banach94@gmail.com>
2022-10-16 10:48:32 +0200

    Added button functionality, images show according to choices and scoreseet.

--- app.js
+++ app.js
@@ -2,26 +2,78 @@
 const playerScoreDisplay = document.querySelector(".player-score");
 const computerScoreDisplay = document.querySelector(".computer-score");
 const textWinner = document.querySelector(".text-winner");
-const playerChoiceImg = document.querySelector(".player-img");
-const computerChoiceImg = document.querySelector(".computer-img");
+const playerChoiceImg = document.querySelector("#player-img");
+const computerChoiceImg = document.querySelector("#computer-img");
 const rockBtn = document.querySelector(".rock-btn");
 const paperBtn = document.querySelector(".paper-btn");
 const scissorsBtn = document.querySelector(".scissors-btn");
 
 // ## Variables:
 
+let playerScore = 0;
+let computerScore = 0;
+let playerChoice;
+
+// ## Code:
+
+rockBtn.addEventListener ('click', (e) => {
+    playerChoice = 'rock';
+    let winner = playRound(playerChoice);
+
+    displayPlayerImg(playerChoice); // changes image of the players choice 
+    displayWinner(winner);
+    
+    if (winner === 'player wins') {
+        playerScore++;
+    }
+    if (winner === 'computer wins') {
+        computerScore++;
+    }
+
+    updateScore();
+});
+
+paperBtn.addEventListener ('click', () => {
+    playerChoice = 'paper';
+    let winner = playRound(playerChoice);
+
+    displayPlayerImg(playerChoice); // changes image of the players choice 
+    displayWinner(winner);
+    
+    if (winner === 'player wins') {
+        playerScore++;
+    }
+    if (winner === 'computer wins') {
+        computerScore++;
+    }
+
+    updateScore();
+});
+
+scissorsBtn.addEventListener ('click', () => {
+    playerChoice = 'scissors';
+    let winner = playRound(playerChoice);
+
+    displayPlayerImg(playerChoice); // changes image of the players choice 
+    displayWinner(winner);
+    
+    if (winner === 'player wins') {
+        playerScore++;
+    }
+    if (winner === 'computer wins') {
+        computerScore++;
+    }
+
+    updateScore();
+});
 
 // ##Functions:
 
-// plays entire game
-function playRockPaperScissors() {
-    playRound();
-}
-
 // plays single round, returns "draw", "player wins" or "computer wins"
-function playRound() {
+function playRound(playerChoice) {
     let computerChoice = getComputerChoice();
-    let playerChoice = getPlayerChoice();
+
+    displayComputerImg(computerChoice); // changes image of the computer choice.
 
     if (playerChoice === computerChoice) {
         return "draw";
@@ -46,7 +98,6 @@ function playRound() {
     }
 }
 
-
 // returns rock paper or scissors 
 function getComputerChoice() {
     let randomNumber = getRandomNumber(3);
@@ -67,8 +118,49 @@ function getRandomNumber(max) {
     return Math.floor(Math.random() * max) + 1;
 }
 
-function getPlayerChoice() {
-    let playerChoice = getComputerChoice(); //local scope variable
-    return playerChoice;
+function displayWinner(winner) {
+    textWinner.innerText = `${winner}`
+    if (winner === "player wins") {
+        textWinner.style.color = "green"
+    }
+    if (winner === "computer wins") {
+        textWinner.style.color = "red"
+    }
+    if (winner === "draw") {
+        textWinner.style.color = "yellow"
+    }
 }
 
+function updateScore() {
+    playerScoreDisplay.innerText = `Player: ${playerScore}`;
+    computerScoreDisplay.innerText = `Computer: ${computerScore}`;
+}
+
+function displayPlayerImg(playerChoice) {
+    if (playerChoice === 'rock') {
+        playerChoiceImg.src = "/img/rock.png"
+        playerChoiceImg.alt = "Rock."
+    }
+    if (playerChoice === 'paper') {
+        playerChoiceImg.src = "/img/paper.png"
+        playerChoiceImg.alt = "Paper."
+    }
+    if (playerChoice === 'scissors') {
+        playerChoiceImg.src = "/img/scissors.png"
+        playerChoiceImg.alt = "Scissors."  
+    }
+}
+function displayComputerImg(computerChoice) {
+    if (computerChoice === 'rock') {
+        computerChoiceImg.src = "/img/rock.png"
+        computerChoiceImg.alt = "Rock."
+    }
+    if (computerChoice === 'paper') {
+        computerChoiceImg.src = "/img/paper.png"
+        playerChoiceImg.alt = "Paper."
+    }
+    if (computerChoice === 'scissors') {
+        computerChoiceImg.src = "/img/scissors.png"
+        playerChoiceImg.alt = "Scissors."  
+    }
+}
